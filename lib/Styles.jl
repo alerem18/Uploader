d8d3c2afeba18b62e78d0d8dda83f3190ebcdf69
Amin <alerem18@gmail.com>
2023-05-27 11:46:12 -0700

    added css styles to use on page

--- lib/Styles.jl
+++ lib/Styles.jl
@@ -2,10 +2,6 @@ module Styles
     using Stipple, StippleUI
     css() = style(
         """
-            .container{
-                height: 100%;
-                background-color: blue;
-            }
             .stipple-core body{
                 
                 
